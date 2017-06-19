64a0b0f74624cb5bda8fc319ca7b36706b778a78
Karl <alcalarenzo94@gmail.com>
2017-06-18 23:23:08 -0500

    Se actualiza el script.

--- Postgres/DB_TrackingSafe.sql
+++ Postgres/DB_TrackingSafe.sql
@@ -2,10 +2,43 @@
 -- TLB: TABLA
 
 create table tlbVehiculo(
+codigo serial primary key,
+conductor varchar(8) REFERENCES tlbConductor (dni),
+placa varchar(6),
 marca varchar(20),
 modelo varchar(20),
-año integer check (año>99999 and año<=9999999),
+año integer check (año>999 and año<=9999),
 color varchar(20),
 comentarios varchar(100)
 );
+
+insert into tlbVehiculo(conductor,placa,marca,modelo,año,color,comentarios) values('12345678','A2O602','Toyota','Corolla',2000,'Blanco','');
+
+drop table tlbVehiculo
+drop table tlbVehiculoHistorial
+drop table tlbConductor
+
+create table tlbVehiculoHistorial(
+codigoVehiculo integer REFERENCES tlbVehiculo (codigo),
+fecha timestamp,
+velocidad integer,
+accx real,
+accy real,
+accz real,
+longitud real,
+latitud real
+);
+
+insert into tlbConductor values ('Juanito','Perez Perez','12345678',22,'ABCDEFGHI')
+
+create table tlbConductor(
+nombres varchar(20),
+apellidos varchar(20),
+dni varchar(8) primary key,
+edad integer,
+licencia varchar(9)
+)
+
+
+
 
